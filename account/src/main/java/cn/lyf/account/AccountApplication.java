3d81c199206f116d1031c71588362ba7bf3bd60d
leiyafeng <leiyafeng@ithro.com.cn>
2018-12-03 17:05:15 +0800

    更新

--- account/src/main/java/cn/lyf/account/AccountApplication.java
+++ account/src/main/java/cn/lyf/account/AccountApplication.java
@@ -2,15 +2,20 @@ package cn.lyf.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement//开启事务管理注解
 @EnableScheduling//定时任务
-public class AccountApplication {
+public class AccountApplication  extends SpringBootServletInitializer {
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AccountApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
